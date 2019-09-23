a5dd3c5e737a7d10147141c26230c6dfc9e6c220
Christian Wyglendowski <christian@dowski.com>
2019-09-22 21:06:03 -0400

    Both players can now build towers, with animations

--- stackytower.py
+++ stackytower.py
@@ -2,32 +2,71 @@
 WIDTH = 600
 HEIGHT = 800
 
+HALF_WIDTH = WIDTH / 2
+PLAYER_1_X = HALF_WIDTH - HALF_WIDTH / 2
+PLAYER_2_X = HALF_WIDTH + HALF_WIDTH / 2
+
 BLOCK_BASIC = 'block_basic'
 
+
 def make_block(block_type):
-    return Actor(block_type)
+    return Actor(block_type, pos=(WIDTH / 2, 0))
 
-def make_player():
-    return {'tower': [], 'blocks': [], 'chosen_block': None}
 
-player1 = make_player()
-player2 = make_player()
+def make_player(x):
+    return {
+        'tower': [],
+        'inventory': [
+            make_block(BLOCK_BASIC),
+            make_block(BLOCK_BASIC),
+            make_block(BLOCK_BASIC)
+        ],
+        'selected_block': None,
+        'x': x,
+    }
+
+
+player1 = make_player(PLAYER_1_X)
+player2 = make_player(PLAYER_2_X)
+
 
 def draw():
     screen.clear()
     for player in [player1, player2]:
         for block in player['tower']:
             block.draw()
+        if player['selected_block']:
+            player['selected_block'].draw()
 
 
 def update():
     pass
 
-def on_mouse_down(pos):
-    add_block_to_tower(player1, BLOCK_BASIC)
 
-def add_block_to_tower(player, block_type):
-    block = make_block(block_type)
-    block.y = HEIGHT - (len(player['tower']) * block.height + block.height / 2)
-    block.x = WIDTH / 2
-    player['tower'].append(block)
+def on_key_down(key):
+    if key == keys.A:
+        drop_block(player1, select_block(player1))
+    if key == keys.K:
+        drop_block(player2, select_block(player2))
+
+
+
+def select_block(player):
+    block = player['inventory'].pop()
+    block.x = player['x']
+    player['selected_block'] = block
+    player['inventory'].insert(0, make_block(BLOCK_BASIC))
+    return player['selected_block']
+
+
+def drop_block(player, block):
+    y = HEIGHT - (len(player['tower']) * block.height + block.height / 2)
+    animate(block, y=y, on_finished=add_dropped_blocks_to_tower)
+
+
+def add_dropped_blocks_to_tower():
+    for player in [player1, player2]:
+        if not player['selected_block']:
+            continue
+        player['tower'].append(player['selected_block'])
+        player['selected_block'] = None
