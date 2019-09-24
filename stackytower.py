7aadd20369e666b2e7ee5ff414001b97fcf95377
Eli Wyglendowski <eli@dowski.com>
2019-09-24 18:47:45 -0400

    block inventory added and new image selected_block

--- stackytower.py
+++ stackytower.py
@@ -7,10 +7,17 @@ HEIGHT = 640
 # in the lists.
 tower1 = []
 tower2 = []
-
-# This bit of math gives us X values to evenly places the towers on the screen.
+cannon = Actor('cannon')
+small_shield = Actor('small_shield')
+basic = Actor('basic')
+inventory = [cannon, small_shield, basic]
+picture = ['cannon', 'small_shield', 'basic']
+selcted_block = 1
 TOWER1_X = WIDTH // 4
 TOWER2_X = TOWER1_X * 3
+inventory[1].pos = (TOWER1_X, 50)
+inventory[0].pos = (inventory[1].x - inventory[1].width, inventory[1].y)
+inventory[2].pos = (inventory[1].x + inventory[1].width, inventory[1].y)
 
 def draw():
     screen.clear()
@@ -18,14 +25,20 @@ def draw():
         block.draw()
     for block in tower2:
         block.draw()
+    for block in inventory:
+        block.draw()
+
+
 
 def update():
     pass
 
+
 def on_key_down(key):
     # When the S key is pressed, add a block for player 1
+    global selcted_block
     if key == keys.S:
-        block = Actor('block_basic')
+        block = inventory[selcted_block] = Actor(picture[selcted_block])
         block.x = TOWER1_X
         block.y = HEIGHT - block.height // 2
         tower1.append(block)
@@ -37,3 +50,17 @@ def on_key_down(key):
         block.y = HEIGHT - block.height // 2
         tower2.append(block)
         print("Player 2 tower has {} blocks".format(len(tower2)))
+def on_key_up(key):
+    global selcted_block
+    if key == keys.A:
+        if selcted_block > 0:
+            inventory[1].pos = (inventory[1].x - 32, 50)
+            inventory[0].pos = (inventory[1].x - inventory[1].width, inventory[1].y)
+            inventory[2].pos = (inventory[1].x + inventory[1].width, inventory[1].y)
+            selcted_block -= 1
+    if key == keys.D:
+        if selcted_block < 2:
+            inventory[1].pos = (inventory[1].x + 32, 50)
+            inventory[0].pos = (inventory[1].x - inventory[1].width, inventory[1].y)
+            inventory[2].pos = (inventory[1].x + inventory[1].width, inventory[1].y)
+            selcted_block += 1
