7740f291ac9d9bd31fdaa9328405e15c276d60f0
Christian Wyglendowski <christian@dowski.com>
2019-09-25 11:37:29 -0400

    Docs and comments and formatting

--- stackytower.py
+++ stackytower.py
@@ -1,8 +1,12 @@
-import random
 # Stacky Tower
+import random
 
 WIDTH = 480
 HEIGHT = 640
+
+# These are the x coordinates for the towers on the screen.
+TOWER1_X = WIDTH // 4
+TOWER2_X = TOWER1_X * 3
 
 # In this game there are two players who race to build a tower of blocks.
 # Each tower will be represented by a list created here. We can put blocks
@@ -12,18 +16,29 @@ tower2 = []
 cannon = Actor('cannon')
 small_shield = Actor('small_shield')
 basic = Actor('basic')
+
+# This is the starting block inventory for the player. These blocks may
+# be dropped onto the tower.
 inventory = [cannon, small_shield, basic]
-picture = ['cannon', 'small_shield', 'basic', 'shotgun', 'large_shield']
-all_blocks = [cannon, small_shield, basic]
-selected_block = 1
-new_block = 0
-TOWER1_X = WIDTH // 4
-TOWER2_X = TOWER1_X * 3
-selected = Actor('selected_block',(TOWER1_X, 50))
 inventory[1].pos = (TOWER1_X, 50)
 inventory[0].pos = (inventory[1].x - inventory[1].width, inventory[1].y)
 inventory[2].pos = (inventory[1].x + inventory[1].width, inventory[1].y)
+
+# These are the images of all available blocks.
+picture = ['cannon', 'small_shield', 'basic', 'shotgun', 'large_shield']
+
+# This represents the currently selected block - it will be changed as
+# the player changes block selection.
+selected_block = 1
+
+# This is used to indicate which block is selected on the screen.
+selected = Actor('selected_block', (TOWER1_X, 50))
+
+# When player 1 drops a block, this variable will hold its Actor.
 player1_falling_block = None
+
+# Some debugging information - holds the duration that it took the last
+# block to fall.
 last_fall_duration = 0.0
 
 # In the game, press / to show debug info
@@ -40,9 +55,12 @@ def draw():
     if player1_falling_block:
         player1_falling_block.draw()
     if debug:
-        screen.draw.text("inventory: {}".format(", ".join(i.image for i in inventory)), (10, 80))
-        screen.draw.text("selected_block: {}".format(selected_block), (10, 100))
-        screen.draw.text("last_fall_duration: {}".format(last_fall_duration), (10, 120))
+        screen.draw.text("inventory: {}".format(
+            ", ".join(i.image for i in inventory)), (10, 80))
+        screen.draw.text("selected_block: {}".format(
+            selected_block), (10, 100))
+        screen.draw.text("last_fall_duration: {}".format(
+            last_fall_duration), (10, 120))
     selected.draw()
 
 
@@ -50,6 +68,7 @@ def update():
     pass
 
 def replace_block():
+    """Replaces the previously selected block with a new random block."""
     new_block_image = random.choice(picture)
     new_block = Actor(new_block_image)
     new_block.x = TOWER1_X
@@ -57,15 +76,18 @@ def replace_block():
     inventory[selected_block] = new_block
 
 def drop_block():
+    """Animates the dropping action of the selected block."""
     global player1_falling_block, last_fall_duration
     block = inventory[selected_block]
     block.x = TOWER1_X
     target_y = HEIGHT - len(tower1) * block.height - block.height // 2
     last_fall_duration = duration = 1.0 * (target_y / HEIGHT)
-    animate(block, duration=duration, y=target_y, tween='bounce_end', on_finished=stop_dropping)
+    animate(block, duration=duration, y=target_y, tween='bounce_end',
+            on_finished=stop_dropping)
     player1_falling_block = block
 
 def stop_dropping():
+    """Adds the block that just fell to the tower."""
     global player1_falling_block
     tower1.append(player1_falling_block)
     player1_falling_block = None
@@ -95,8 +117,12 @@ def on_key_up(key):
             switch_selected_block(1)
 
 def switch_selected_block(direction):
+    """Given a direction of either 1 or -1, changes the selected block."""
     global selected_block
-    inventory[1].pos = (inventory[1].x + (direction * inventory[1].width), inventory[1].y)
-    inventory[0].pos = (inventory[0].x + (direction * inventory[0].width), inventory[0].y)
-    inventory[2].pos = (inventory[2].x + (direction * inventory[2].width), inventory[2].y)
+    inventory[1].pos = (
+        inventory[1].x + (direction * inventory[1].width), inventory[1].y)
+    inventory[0].pos = (
+        inventory[0].x + (direction * inventory[0].width), inventory[0].y)
+    inventory[2].pos = (
+        inventory[2].x + (direction * inventory[2].width), inventory[2].y)
     selected_block -= direction
