6ef53f9deaa90dc440a5ced8cfc11b9ecda03854
Christian Wyglendowski <christian@dowski.com>
2019-09-26 20:56:22 -0400

    Adds support for winning the game
    The first tower to cross the finish line first wins. When the game is
    over further key presses don't do anything.

--- stackytower.py
+++ stackytower.py
@@ -5,6 +5,14 @@ import pygame.transform
 WIDTH = 480
 HEIGHT = 640
 
+# Some of the block images are bigger than this, but the actual block
+# in the game is considered 32 pixels high (this allows some blocks to
+# visually overlap with others).
+BLOCK_HEIGHT = 32
+
+# The y coordinate on the screen where the finish line should be
+FINISH_LINE = 128
+
 class Player:
     pass
 
@@ -43,6 +51,9 @@ player2.inventory = make_inventory(player2.towerx)
 
 # These are the images of all available blocks.
 icons = ['cannon_icon', 'small_shield_icon', 'basic', 'shotgun_icon']
+
+# This dictionary maps icon images to full block images - for some
+# the icon is the full block.
 full_block_map = {
     'cannon_icon': 'cannon',
     'small_shield_icon': 'small_shield',
@@ -69,6 +80,9 @@ last_fall_duration = 0.0
 
 # In the game, press / to show debug info
 debug = False
+
+# The winner of the game
+winner = None
 
 def draw():
     screen.blit('background', (0, 0))
@@ -101,6 +115,18 @@ def draw():
             last_fall_duration), (10, 80))
     selector1.draw()
     selector2.draw()
+    # draw the finish line
+    for i in range(8, WIDTH, 32):
+        screen.draw.line(
+            (i, FINISH_LINE), (i + 16, FINISH_LINE), (255, 32, 32))
+    if winner:
+        if winner is player1:
+            winner_name = "Player 1"
+        else:
+            winner_name = "Player 2"
+        screen.draw.text("{} is the winner!".format(winner_name),
+            center=(WIDTH // 2, HEIGHT // 2), color=(255, 128, 128),
+            fontsize=32)
 
 def replace_block(player):
     """Replaces the previously selected block with a new random block."""
@@ -118,7 +144,8 @@ def drop_block(player):
     block.image = full_block_map[block.image]
     if player.facing_left:
         flip_actor_image(block)
-    block.target_y = HEIGHT - len(player.tower) * 32 - 32 // 2
+    tower_height = len(player.tower) * BLOCK_HEIGHT
+    block.target_y = HEIGHT - tower_height - BLOCK_HEIGHT // 2
     last_fall_duration = duration = 1.0 * (block.target_y / HEIGHT)
     animate(block, duration=duration, y=block.target_y, tween='bounce_end',
             on_finished=stop_dropping)
@@ -126,18 +153,29 @@ def drop_block(player):
 
 def stop_dropping():
     """Adds the block that just fell to the tower."""
+    global winner
     if player1.falling_block and (
             player1.falling_block.y == player1.falling_block.target_y):
         player1.tower.append(player1.falling_block)
         player1.falling_block = None
+        if is_winner(player1):
+            winner = player1
     if player2.falling_block and (
             player2.falling_block.y == player2.falling_block.target_y):
         player2.tower.append(player2.falling_block)
         player2.falling_block = None
+        if is_winner(player2):
+            winner = player2
+
+def is_winner(player):
+    return not winner and (
+        HEIGHT - len(player.tower) * BLOCK_HEIGHT < FINISH_LINE)
 
 def on_key_up(key):
     # When the S key is pressed, add a block for player 1
     global debug
+    if winner:
+        return
     if key == keys.S and not player1.falling_block:
         drop_block(player1)
         replace_block(player1)
